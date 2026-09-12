024bfb767f97d742bcd1aea2b29642941cb6f0ed
matrixhubtokypill-pixel <matrixhub.tokypill@gmail.com>
2026-09-13 06:11:30 +0900

    Refactor TriggerBind assignment logic

--- hdcmffa.lua
+++ hdcmffa.lua
@@ -6775,7 +6775,7 @@ if not Processed then
 
     local SilentBind      = (not isAutoMode) and toKeyCode(K['Silent Aim'] and K['Silent Aim']['Target Bind']) or nil
     local AssistBind   = toKeyCode(K['Aim Assist'] and K['Aim Assist']['Bind'])
-    local TriggerBind     = (not isAutoMode) and toKeyCode(K['Triggerbot'] and K['Triggerbot']['Bind'])          or nil
+    local TriggerBind     = toKeyCode(K['Triggerbot'] and K['Triggerbot']['Bind'])
 
     if SilentBind and Input.KeyCode == SilentBind then
         Script.Locals.SP = not Script.Locals.SP
